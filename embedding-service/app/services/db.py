5fbc1687e0032bdf78952ef3c874453f4fc954e5
50OFF <nikita.poltinnikov@yandex.ru>
2026-02-08 17:19:11 +0000

    Table creating in DB service

--- embedding-service/app/services/db.py
+++ embedding-service/app/services/db.py
@@ -2,6 +2,7 @@
 
 import asyncpg
 from pgvector.asyncpg import register_vector
+from asyncpg import exceptions
 
 class DataBase():
     def __init__(self, host: str, port: int, user: str, password: str, db: str):
@@ -23,13 +24,41 @@ class DataBase():
             init=register_vector
         )
 
-    async def insert_embedding(self, chunk_id: str, file_id: str, user_id: str, text: str, embedding: list):
-        "Insert new row in embeddings table."
+    async def create_embeddings_table(self):
+        "Create embeddings table."
         async with self.pool.acquire() as connection:
             await connection.execute(
                 """
-                INSERT INTO embeddings (chunk_id, file_id, user_id, text, embedding)
-                VALUES ($1, $2, $3, $4, $5)
-                """,
-                chunk_id, file_id, user_id, text, embedding
+                CREATE TABLE IF NOT EXISTS embeddings (
+                    chunk_id TEXT PRIMARY KEY,
+                    file_id TEXT NOT NULL,
+                    user_id TEXT NOT NULL,
+                    text TEXT,
+                    embedding vector(1536)
+                );
+                """
             )
+
+    async def insert_embedding(self, chunk_id: str, file_id: str, user_id: str, text: str, embedding: list):
+        "Insert new row in embeddings table."
+        try:
+            async with self.pool.acquire() as connection:
+                await connection.execute(
+                    """
+                    INSERT INTO embeddings (chunk_id, file_id, user_id, text, embedding)
+                    VALUES ($1, $2, $3, $4, $5)
+                    """,
+                    chunk_id, file_id, user_id, text, embedding
+                )
+        
+        except exceptions.UndefinedTableError:
+            await self.create_embeddings_table()
+
+            async with self.pool.acquire() as connection:
+                await connection.execute(
+                    """
+                    INSERT INTO embeddings (chunk_id, file_id, user_id, text, embedding)
+                    VALUES ($1, $2, $3, $4, $5)
+                    """,
+                    chunk_id, file_id, user_id, text, embedding
+                )
